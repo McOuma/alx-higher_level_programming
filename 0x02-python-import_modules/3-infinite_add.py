d161b18722a1f2e8018ccf9dac1e9382f144cca4
BonOuma <mcouma.mugah@gmail.com>
2023-06-08 13:58:08 +0300

    correction

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """Print the addition of all arguments."""
     import sys
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    else:
+        print("{:d} argument".format(len(sys.argv) - 1), end="")
+        if len(sys.argv) > 2:
+            print("s", end="")
+        print(":")
+        for i in range(1, len(sys.argv)):
+            print("{:d}: {:s}".format(i, sys.argv[i]))
