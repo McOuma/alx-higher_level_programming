82d29ebe277e1cc9fbb202946787d77f4281d0df
McOuma <oumamugah@gmail.com>
2023-06-08 20:25:37 +0300

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
+
 import hidden_4
 
-
-def discovr():
-    name = dir(hidden_4)
-    for i in name:
-        if i[:2] != '__':
-            print("{:s}".format(i))
-
-
 if __name__ == "__main__":
-    discovr()
+    for content in dir(hidden_4):
+        if content[:2] != "__":
+            print("{:s}".format(content))
