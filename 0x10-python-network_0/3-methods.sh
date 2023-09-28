39bc0572ca0c032d8b0e70f8e10973a978635e03
BonOuma <mcouma.mugah@gmail.com>
2023-09-28 17:44:58 +0300

    delete

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#  a Bash script that takes in a URL and displays all HTTP methods the server will accept
-curl -si "$1" | awk -F ": " '/Allow/ {print $2}'
+# Select methods
+curl -sI "$1" | grep Allow: | cut -d":" -f2 | sed 's/ //'
